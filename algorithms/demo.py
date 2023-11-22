c44d906e9d2993e84ec99418d2a52288b7891a8a
md55md <zzusywh@163.com>
2023-11-22 09:49:41 +0800

    orginal version

---
+++ algorithms/demo.py
@@ -0,0 +1,46 @@
+from ab.utils import logger
+from ab.utils.algorithm import algorithm
+from ab import app
+
+# 可以使用这种方式载入配置里的数据，然后执行载入模型之类的操作
+model_path = app.config.MODEL_PATH
+logger.info('model path is:', model_path)
+
+
+# 会自动暴露为/api/algorithm/add接口
+@algorithm()
+def add(a: int, b: int) -> int:
+    """
+    一个简单的加法算法示例
+    :param a: 第一个参数
+    :param b: 第二个参数
+    :return:
+    """
+    logger.info("enter algorithm {}, {} ".format(a, b))
+    return a + b
+
+@algorithm()
+def hello_world():
+    return str("hello,world")
+@algorithm()
+def crypto():
+    """
+    读取加密文件
+    :return:
+    """
+    from ab.keys.data import read_text, read_pickle, open_binary
+
+    # with open_binary("hanhao.pkl") as buffer:
+    #    print(1)
+    # return "aa"
+    return read_text("resource/mydata")
+    # return str(read_pickle("resource/hanhao"))[:20]
+
+
+@algorithm()
+def compress() -> int:
+    """
+    :return:
+    """
+    return "this is a hello world text file, glad to meet you, bye ~this is a hello world text file, glad to meet you, bye this is a hello world text file, glad to meet you, bye this is a hello world text file, glad to meet you, bye this is a hello world text file, glad to meet you, bye this is a hello world text file, glad to meet you, bye this is a hello world text file, glad to meet you, bye this is a hello world text file, glad to meet you, bye this is a hello world text file, glad to meet you, bye this is a hello world text file, glad to meet you, bye this is a hello world text file, glad to meet you, bye this is a hello world text file, glad to meet you, bye this is a hello world text file, glad to meet you, bye this is a hello world text file, glad to meet you, bye "
+
---
+++ config/config.py
@@ -0,0 +1,21 @@
+"""
+基础配置
+"""
+MODEL_PATH = '/tmp/xxxx'
+
+# TESTING = True
+# DEBUG = True
+
+# 这个值只能由英文字母构成，不能包含任何符号！
+APP_NAME = 'simple'
+
+HOST = '0.0.0.0'
+
+PRINT_RESULT = False
+LOG_LEVEL = 'DEBUG'
+
+
+preload_app = False
+
+# 是否开启存活检查
+ENABLE_LIVENESS_PROB = False
